b0ab8f8f6c4177b7f7746a47493f96c37d00e3e0
sowish23 <soso2266@naver.com>
2021-06-09 17:07:42 +0900

    addAddress

--- findMarker.py
+++ findMarker.py
@@ -23,18 +23,23 @@ ret, thr = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU) 
 
 contours, _ = cv2.findContours(thr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+address = ''
 
 for cont in contours:
     approx = cv2.approxPolyDP(cont, cv2.arcLength(cont, True) * 0.02, True)
     vtc = len(approx)
     if vtc == 3:
         setLabel(img_dia, cont, 'Tri')
+        address = '서울시 성북구 성북로 11길 25'
     elif vtc == 4:
         setLabel(img_dia, cont, 'Rec')
+        address = '서울시 성북구 성북로 12길'
     elif vtc == 5:
         setLabel(img_dia, cont, 'Diamond')
+        address = '서울시 성북구 성북로 13길 13'
     elif vtc == 10:
         setLabel(img_dia, cont, 'Star')
+        address = '서울시 성북구 성북로 13길 8'
     else:
         area = cv2.contourArea(cont)
         _, radius = cv2.minEnclosingCircle(cont)
@@ -49,7 +54,17 @@ cv2.imshow('binary', thr)
 img_text = img_resize[:, 150:]
 text = pytesseract.image_to_string(img_text,lang='eng')
 
-print(text)
+if text.rstrip() == 'ABCD' :
+    address += ' 2층 회의실 A구역'
+elif text.rstrip() == 'ABCE' : 
+    address += ' 2층 탕비실'
+elif text.rstrip() == 'ABCF' : 
+    address += ' 2층 장비실'
+elif text.rstrip() == 'ABCG' : 
+    address += ' 2층 대표실'
+elif text.rstrip() == 'ABCH' : 
+    address += ' 2층 연구실'
+print(address)
 
 # subprocess.call("tesseract marker_example2.png output -l eng", shell=True)
 
